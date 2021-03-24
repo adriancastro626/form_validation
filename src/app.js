2981a220b0c10728fa93f1570b7e5da38557d54e
adriancastro626 <adriancastro626@icloud.com>
2021-03-24 05:16:58 +0000

    Formulario de Pago

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let submitButton = document.querySelector("#submitButton");
 
 submitButton.addEventListener("click", function(e) {
-  //VALIDATION FOR CARD NUMBER
+  //Validacion de la TC
 
   let inputCardNumber = document.getElementById("inputCardNumber");
 
@@ -9,20 +9,19 @@ submitButton.addEventListener("click", function(e) {
     let cardNumberAlert = document.createElement("div");
     cardNumberAlert.className = "text-danger";
     cardNumberAlert.innerHTML =
-      "<small>Please enter valid card number.</small>";
+      "<small>Ingrese un numero de Tarjeta Valido</small>";
     document.getElementById("cardNumberDiv").appendChild(cardNumberAlert);
     inputCardNumber.className = "form-control border border-danger";
     e.preventDefault();
   }
 
   if (
-    inputCardNumber.value.length !== 16 &&
+    inputCardNumber.value.length !== 18 &&
     inputCardNumber.value.length !== 0
   ) {
     let cardNumberAlert = document.createElement("div");
     cardNumberAlert.className = "text-danger";
-    cardNumberAlert.innerHTML =
-      "<small>Card number should be 16 digits.</small>";
+    cardNumberAlert.innerHTML = "<small>Ingrese 18 digitos</small>";
     document.getElementById("cardNumberDiv").appendChild(cardNumberAlert);
     inputCardNumber.className = "form-control border border-danger";
     e.preventDefault();
@@ -31,21 +30,20 @@ submitButton.addEventListener("click", function(e) {
   if (isNaN(inputCardNumber.value)) {
     let cardNumberAlert = document.createElement("div");
     cardNumberAlert.className = "text-danger";
-    cardNumberAlert.innerHTML =
-      "<small>Card number should only contain numbers.</small>";
+    cardNumberAlert.innerHTML = "<small>Ingrese solo numeros</small>";
     document.getElementById("cardNumberDiv").appendChild(cardNumberAlert);
     inputCardNumber.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  // VALIDATION FOR CVC NUMBER
+  // Validacion de CVC
 
   let inputCVCNumber = document.getElementById("inputCVCNumber");
 
   if (inputCVCNumber.value === "") {
     let cvcNumberAlert = document.createElement("div");
     cvcNumberAlert.className = "text-danger";
-    cvcNumberAlert.innerHTML = "<small>Please enter valid CVC number.</small>";
+    cvcNumberAlert.innerHTML = "<small>Ingrese un CVC Valido</small>";
     document.getElementById("cvcNumberDiv").appendChild(cvcNumberAlert);
     inputCVCNumber.className = "form-control border border-danger";
     e.preventDefault();
@@ -54,7 +52,7 @@ submitButton.addEventListener("click", function(e) {
   if (inputCVCNumber.value.length !== 4 && inputCVCNumber.value.length !== 0) {
     let cvcNumberAlert = document.createElement("div");
     cvcNumberAlert.className = "text-danger";
-    cvcNumberAlert.innerHTML = "<small>CVC number should be 4 digits.</small>";
+    cvcNumberAlert.innerHTML = "<small>CVC deberia tener 4 digitos</small>";
     document.getElementById("cvcNumberDiv").appendChild(cvcNumberAlert);
     inputCVCNumber.className = "form-control border border-danger";
     e.preventDefault();
@@ -63,87 +61,85 @@ submitButton.addEventListener("click", function(e) {
   if (isNaN(inputCVCNumber.value)) {
     let cvcNumberAlert = document.createElement("div");
     cvcNumberAlert.className = "text-danger";
-    cvcNumberAlert.innerHTML =
-      "<small>CVC number should only contain numbers.</small>";
+    cvcNumberAlert.innerHTML = "<small>Ingrese solo numeros</small>";
     document.getElementById("cvcNumberDiv").appendChild(cvcNumberAlert);
     inputCVCNumber.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  // VALIDATION FOR AMOUNT
+  // Validacion del monto
 
   let inputAmount = document.getElementById("inputAmount");
 
   if (inputAmount.value === "" || isNaN(inputAmount.value)) {
     let amountAlert = document.createElement("div");
     amountAlert.className = "text-danger";
-    amountAlert.innerHTML = "<small>Please enter valid amount.</small>";
+    amountAlert.innerHTML = "<small>Agregue un monto superior a $10.00</small>";
     document.getElementById("amountDiv").appendChild(amountAlert);
     inputAmount.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  // VALIDATION FOR FIRST NAME
+  // Validacion nombre
 
   let inputFirstName = document.getElementById("inputFirstName");
 
   if (inputFirstName.value === "") {
     let nameAlert = document.createElement("div");
     nameAlert.className = "text-danger";
-    nameAlert.innerHTML = "<small>Please enter valid name.</small>";
+    nameAlert.innerHTML = "<small>Ingrese un nombre valido</small>";
     document.getElementById("nameDiv").appendChild(nameAlert);
     inputFirstName.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  // VALIDATION FOR LAST NAME
+  // Validacion del apellido
 
   let inputLastName = document.getElementById("inputLastName");
 
   if (inputLastName.value === "") {
     let lastnameAlert = document.createElement("div");
     lastnameAlert.className = "text-danger";
-    lastnameAlert.innerHTML = "<small>Please enter valid lastname.</small>";
+    lastnameAlert.innerHTML = "<small>Ingrese un nombre valido</small>";
     document.getElementById("lastnameDiv").appendChild(lastnameAlert);
     inputLastName.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  //VALIDATION FOR CITY
+  //Validacion de la ciudad
 
   let inputCity = document.getElementById("inputCity");
 
   if (inputCity.value === "") {
     let cityAlert = document.createElement("div");
     cityAlert.className = "text-danger";
-    cityAlert.innerHTML = "<small>Please enter valid city.</small>";
+    cityAlert.innerHTML = "<small>Ingrese una Ciudad Valida</small>";
     document.getElementById("cityDiv").appendChild(cityAlert);
     inputCity.className = "form-control border border-danger";
     e.preventDefault();
   }
 
-  //VALIDATION FOR STATE
+  //Validacion de Canton
 
   let inputState = document.getElementById("inputState");
 
   if (inputState.value === "") {
     let stateAlert = document.createElement("div");
     stateAlert.className = "text-danger";
-    stateAlert.innerHTML = "<small>Please select state.</small>";
+    stateAlert.innerHTML = "<small>Elija un Canton Valido</small>";
     document.getElementById("stateDiv").appendChild(stateAlert);
     inputState.className = "form-control custom-select border border-danger";
     e.preventDefault();
   }
 
-  //VALIDATION FOR POSTAL CODE
+  //Validacion codigo postal
 
   let inputPostalCode = document.getElementById("inputPostalCode");
 
   if (inputPostalCode.value === "") {
     let postalCodeAlert = document.createElement("div");
     postalCodeAlert.className = "text-danger";
-    postalCodeAlert.innerHTML =
-      "<small>Please enter valid postal code.</small>";
+    postalCodeAlert.innerHTML = "<small>Ingrese codigo postal valido</small>";
     document.getElementById("postalCodeDiv").appendChild(postalCodeAlert);
     inputPostalCode.className = "form-control border border-danger";
     e.preventDefault();
@@ -152,8 +148,7 @@ submitButton.addEventListener("click", function(e) {
   if (isNaN(inputPostalCode.value)) {
     let postalCodeAlert = document.createElement("div");
     postalCodeAlert.className = "text-danger";
-    postalCodeAlert.innerHTML =
-      "<small>Postal Code should only contain numbers.</small>";
+    postalCodeAlert.innerHTML = "<small>Ingrese solo numeros</small>";
     document.getElementById("postalCodeDiv").appendChild(postalCodeAlert);
     inputPostalCode.className = "form-control border border-danger";
     e.preventDefault();
@@ -163,5 +158,5 @@ submitButton.addEventListener("click", function(e) {
 let resetButton = document.querySelector("#resetButton");
 
 resetButton.addEventListener("click", function(e) {
-  document.getElementById("myForm").reset();
+  document.getElementById("myForm").reset(e);
 });
